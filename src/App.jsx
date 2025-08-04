be9a2a2663eb116d1d0d183bab735c183652d0d1
92Ezio48 <vovanew77714@gmail.com>
2025-08-04 20:03:10 +0300

    1. Добавлена верстка Страницы Авторизации, Добавлен AuthContext и AuthProvider, изменен роутинг в App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import './App.css'
-import './assets/styles/globals.scss'
-import Header from './components/layout/Header/Header'
-import Main from './components/Main/Main'
+import "./App.css";
+import "./assets/styles/globals.scss";
+import Header from "./components/layout/Header/Header";
+import Main from "./components/Main/Main";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext/AuthProvider"; // <--- Импортируй свой провайдер
 
 const App = () => {
-	return (
-		<div>
-			<Header />
-			<Main />
-		</div>
-	)
-}
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Header />
+        <Main />
+      </BrowserRouter>
+    </AuthProvider>
+  );
+};
 
-export default App
+export default App;
