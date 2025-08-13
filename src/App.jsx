db2c50308ff4df7ff5cf8121bbed6e2e99acab18
WiperZ69 <56507851+WiperZ69@users.noreply.github.com>
2025-08-13 15:20:42 +0800

    Revert "1. Добавлена верстка Страницы Авторизации, Добавлен AuthContext и Aut…"

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
-import "./App.css";
-import "./assets/styles/globals.scss";
-import Header from "./components/layout/Header/Header";
-import Main from "./components/Main/Main";
-import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./contexts/AuthContext/AuthProvider"; // <--- Импортируй свой провайдер
+import './App.css'
+import './assets/styles/globals.scss'
+import Header from './components/layout/Header/Header'
+import Main from './components/Main/Main'
 
 const App = () => {
-  return (
-    <AuthProvider>
-      <BrowserRouter>
-        <Header />
-        <Main />
-      </BrowserRouter>
-    </AuthProvider>
-  );
-};
+	return (
+		<div>
+			<Header />
+			<Main />
+		</div>
+	)
+}
 
-export default App;
+export default App
